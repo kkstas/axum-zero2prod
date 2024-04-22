2c37390a19587d5a099012b85a304fb06dfc2959
kkstas <stasiakkamilkrzysztof@gmail.com>
2024-04-22 19:52:51 +0200

    Always bind tests to free address

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,9 @@
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().await;
-
+    let addr = spawn_app().await;
     let client = reqwest::Client::new();
     let response = client
-        .get("http://127.0.0.1:9090/health_check")
+        .get(format!("{}/health_check", addr))
         .send()
         .await
         .expect("Failed to execute a request");
@@ -13,11 +12,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() {
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:9090")
-        .await
-        .unwrap();
+async fn spawn_app() -> String {
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let port = listener.local_addr().unwrap().port();
     let _ = tokio::spawn(async move {
         axum::serve(listener, axum_z2p::run()).await.unwrap();
     });
+    format!("http://127.0.0.1:{}", port)
 }
